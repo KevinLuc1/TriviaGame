b730cc9582d35b641ce5e7fe858d23764364e67f
KevinLuc1 <kevinluc1993@gmail.com>
2017-09-13 15:00:51 -0700

    working game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,29 +11,72 @@ $("#clickHere").click(function(){
 	$(".timer").show();
 })
 
-var timeRemain = 31;
+var timeRemain = 31; // setting this to 31 because the first # user sees on countdown is 30
 var countDown = setInterval(function(){
 	timeRemain--;
 	$("#timeLeft").html(timeRemain + " seconds");
 	if (timeRemain === 0){
 		clearInterval(countDown);
+		$("#submit").click()
 	}
+
 },1000);
 
 
-function check(){
+
+
+
+
+$("#submit").click(function check(){
 	$(".quizDone").show();
+	$(".questions").hide();
+	$(".timer").hide();
 
-	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question1.value;
-	var question3 = document.quiz.question1.value;
-	var correct = 0;
-	var incorrect = 0;
 
-	if (question1 == "1") {
+	var correct = 0
+	var incorrect = 0
+
+
+
+	if (  $("input[value=1]").is(":checked")    ) {
+		correct++;
+		
+		}
+	 
+	if ( $("input[value=7]").is(":checked")) {
+		correct++
+	}
+	if ( $("input[value=11]").is(":checked")) {
+		correct++
+	}
+	if ( $("input[value=16]").is(":checked")) {
 		correct++
 	}
 
+	incorrect = (4 - correct)
 
-}
+	$("#correctAnswer").html("You answered " + correct + " correctly")
+	$("#incorrectAnswer").html("You answered " + incorrect + " incorrectly")
+
+
+})
+
+
+
+
+// function check(){
+// 	$(".quizDone").show();
+
+// 	var question1 = document.quiz.question1.value;
+// 	var question2 = document.quiz.question1.value;
+// 	var question3 = document.quiz.question1.value;
+// 	var correct = 0;
+// 	var incorrect = 0;
+
+// 	if (question1 == "1") {
+// 		correct++
+// 	}
+
+
+// }
 
